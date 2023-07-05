3beb3d4235b7aaf29889a73c2bd040760c18b480
Anchouses <foranngel@gmail.com>
2023-07-05 12:39:19 +0500

    05.07.23 12:40 menu

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -4,9 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import android.text.format.DateFormat
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -40,6 +38,10 @@ class CrimeListFragment: Fragment() {
         ViewModelProviders.of(this)[CrimeListViewModel::class.java]
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
     override fun onCreateView(
         inflater: LayoutInflater,  //наполнение
         container: ViewGroup?,     //
@@ -67,6 +69,24 @@ class CrimeListFragment: Fragment() {
     override fun onDetach(){   //функция жизненного цикла Fragment
         super.onDetach()
         callbacks = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_crime_list, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.new_crime -> {
+                val crime = Crime()
+                crimeListViewModel.addCrime(crime)
+                callbacks?.onCrimeSelected(crime.id)
+                true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+
     }
     private fun updateUI(crimes: List<Crime>){   //
         adapter = CrimeAdapter(crimes)
